c639d80347eccaee02a7f46f4c938500c71cdc97
Alioth <610886764@qq.com>
2024-08-01 14:59:13 +0800

    chore: Update version in CLI to use package.json

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,10 @@
 import { Command } from "commander";
 import { YourAction } from "./index";
+const pkg = require("../package.json");
 const program = new Command();
 
 program
-  .version("0.0.1")
+  .version(pkg.version)
   .description("xxx - A command line tool for xxx.")
   .action(async (options: any) => {
     // write your code here
